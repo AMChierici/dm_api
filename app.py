4abac3d763f9adf45da19f8bde8210ced3bac063
AMChierici <alberto.chierici@nyu.edu>
2021-02-11 15:00:55 +0400

    changed dialogue_manager for posgresql db and added update_avatar to handle multiavatars creation

--- app.py
+++ app.py
@@ -1,33 +1,51 @@
 import json
 from flask import Flask, request
-from haystack.document_store.memory import InMemoryDocumentStore
+from haystack.document_store.faiss import FAISSDocumentStore
 from haystack.retriever.dense import EmbeddingRetriever
 from haystack.utils import print_answers
-import pandas as pd
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+from sqlalchemy_utils import database_exists, create_database
+import pandas as pandas
 import numpy as np
-import pickle
 
 model_path = "deepset/sentence_bert"
+sql_url = "postgresql:///{}?client_encoding=utf8"
+faiss_file_path = "faiss_indices/{}"
 
-infile = open("faiss_indices/margarita.pkl",'rb')
-document_store = pickle.load(infile)
-infile.close()
-
-retriever = EmbeddingRetriever(document_store=document_store, 
-                               embedding_model=model_path, 
-                               use_gpu=False)
+# db = SQLAlchemy()
 
 app = Flask(__name__)
 
-@app.route('/dialogue_manager', methods=['POST'])
+# app.config['SQLALCHEMY_DATABASE_URI'] = sql_url.format("margarita1234")
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+ 
+# db.init_app(app)
+# migrate = Migrate(app, db)
+
+
+@app.route('/dialogue_manager', methods=['GET'])
 
 def dialogue_manager():
-    if request.method == 'POST':
+    if request.method == 'GET':
         raw_data = request.get_json()
         query = raw_data['query']
+        avatar_id = raw_data['avatar_id']
+
+        avatar_sql_url = sql_url.format(avatar_id)
+        avatar_faiss_file_path = faiss_file_path.format(avatar_id)
+
         if query is None:
             return 'Please enter a query', 400
         
+        document_store = FAISSDocumentStore.load(
+                faiss_file_path=avatar_faiss_file_path,
+                sql_url=avatar_sql_url)
+
+        retriever = EmbeddingRetriever(document_store=document_store, 
+                               embedding_model=model_path, 
+                               use_gpu=False)
+
         query_embedding = np.array(
             retriever.embed_queries(texts=query)
         )
@@ -45,6 +63,49 @@ def dialogue_manager():
         }
         json.dumps(result)
         return result
+
+
+
+@app.route('/update_avatar', methods=['POST'])
+
+def update_avatar():
+    if request.method == 'POST':
+
+        raw_data = request.get_json()
+
+        avatar_id = raw_data['avatar_id']
+        question = raw_data['question']
+        answer = raw_data['answer']
+        id_video = raw_data['id_video']
+
+        avatar_sql_url = sql_url.format(avatar_id)
+        avatar_faiss_file_path = faiss_file_path.format(avatar_id)
+
+        if not database_exists(avatar_sql_url):
+            create_database(avatar_sql_url)
+            document_store = FAISSDocumentStore(sql_url=avatar_sql_url)
+        else:
+            document_store = FAISSDocumentStore.load(
+                faiss_file_path=avatar_faiss_file_path,
+                sql_url=avatar_sql_url)
+        
+        retriever = EmbeddingRetriever(document_store=document_store, 
+                               embedding_model=model_path, 
+                               use_gpu=False)
+        docs_to_index = [{
+            'text': question,
+            'answer': answer,
+            'id_video': id_video
+        }]
+
+        document_store.write_documents(docs_to_index)
+        document_store.update_embeddings(
+            retriever=retriever
+            )
+
+        document_store.save(faiss_file_path.format(avatar_id))
+
+        return "UPDATED {}".format(avatar_id)
 
 
 if __name__ == '__main__':
